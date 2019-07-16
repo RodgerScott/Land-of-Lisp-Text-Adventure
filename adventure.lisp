d619d7ab3e0945c0520daf644c184a6ec12b3e7f
RodgerScott <rodger.scott@gmail.com>
2019-07-15 21:23:06 -0500

    added paths

--- adventure.lisp
+++ adventure.lisp
@@ -1,6 +1,18 @@
 (defparameter *nodes* '((living-room
 			 (You are in the living room. A wizard is snoring loudly on the couch.))
 			(garden
-			 (You are ina a beautiful garden. There is a well in front of you.))
+			 (You are in a a beautiful garden. There is a well in front of you.))
 			(attic (You are in the attic. There is a giant welding torch in the corner.))))
 			
+(defun describe-location (location nodes)
+  (cadr (assoc location nodes)))
+
+(defparameter *edges* '((living-room (garden west door)
+			 (attic upstairs ladder))
+			(garden (living-room east door))
+			(attic (living-room downstairs ladder))))
+
+(defun describe-path (edge)
+  `(there is a ,(caddr edge) going ,(cadr edge) from here.))
+		    
+
