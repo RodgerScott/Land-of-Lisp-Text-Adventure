5c8d224a62449521f3c9121159eb7bc8ae14b8e9
RodgerScott <rodger.scott@gmail.com>
2019-07-15 20:45:38 -0500

    beginning the game

---
+++ adventure.lisp
@@ -0,0 +1,6 @@
+(defparameter *nodes* '((living-room
+			 (You are in the living room. A wizard is snoring loudly on the couch.))
+			(garden
+			 (You are ina a beautiful garden. There is a well in front of you.))
+			(attic (You are in the attic. There is a giant welding torch in the corner.))))
+			
