bb936751bfae4fe3057e48b8a1d9c70ec9d01f78
RodgerScott <rodger.scott@gmail.com>
2019-07-16 21:34:15 -0500

    added the describe paths function

--- adventure.lisp
+++ adventure.lisp
@@ -14,3 +14,7 @@
 
 (defun describe-path (edge)
   `(there is a ,(caddr edge) going ,(cadr edge) from here.))
+
+(defun describe-paths (location edges)
+  (apply #'append (mapcar #'describe-path (cdr (assoc
+						location edges)))))
