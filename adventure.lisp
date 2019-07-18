2f2acdf2eeb20458f0026ad48eb593b1082fd584
RodgerScott <rodger.scott@gmail.com>
2019-07-17 21:48:56 -0500

    added objects and object descriptions

--- adventure.lisp
+++ adventure.lisp
@@ -18,3 +18,21 @@
 (defun describe-paths (location edges)
   (apply #'append (mapcar #'describe-path (cdr (assoc
 						location edges)))))
+
+(defparameter *objects* '(whiskey bucket frog chain))
+
+(defparameter *object-locations* '((whiskey living-room)
+				   (bucket living-room)
+				   (chain garden)
+				   (frog garden)))
+
+(defun objects-at (loc objs obj-locs)
+  (labels ((at-loc-p (obj)
+	     (eq (cadr (assoc obj obj-locs)) loc)))
+    (remove-if-not #'at-loc-p objs)))
+
+
+(defun describe-objects (loc objs obj-loc)
+  (labels ((describe-obj (obj)
+	     `(you see a ,obj on the floor.)))
+    (apply #'append (mapcar #'describe-obj (objects-at loc objs obj-loc)))))
